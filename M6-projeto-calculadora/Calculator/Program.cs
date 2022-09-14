64c373a5b3c2b28372b0ca5006326c37e1e03d5d
nicoledbianchin <nicoledbianchin@gmail.com>
2022-09-14 14:47:35 -0300

    modulo 6 - divisao

--- M6-projeto-calculadora/Calculator/Program.cs
+++ M6-projeto-calculadora/Calculator/Program.cs
@@ -9,6 +9,7 @@ namespace Calculator
             Console.Clear();
             Soma();
             Subtracao();
+            Divisao();
         }
 
         static void Soma()
@@ -35,7 +36,21 @@ namespace Calculator
 
             float resultado = valor1 - valor2;
 
-            Console.WriteLine($"\nResultado da subtração: {resultado}");
+            Console.WriteLine($"\nResultado da subtração: {resultado}\n");
+            Console.ReadKey();
+        }
+
+        static void Divisao()
+        {
+            Console.WriteLine("Primeiro valor: ");
+            float valor1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float valor2 = float.Parse(Console.ReadLine());
+
+            float resultado = valor1 / valor2;
+
+            Console.WriteLine($"\nResultado da divisao: {resultado}\n");
             Console.ReadKey();
         }
     }
