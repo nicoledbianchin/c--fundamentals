c3ef9592f991388067736a01e9a539222ec9d2cb
nicoledbianchin <nicoledbianchin@gmail.com>
2022-09-14 15:14:25 -0300

    modulo 6 - multiplicacao

--- M6-projeto-calculadora/Calculator/Program.cs
+++ M6-projeto-calculadora/Calculator/Program.cs
@@ -10,6 +10,7 @@ namespace Calculator
             Soma();
             Subtracao();
             Divisao();
+            Multiplicacao();
         }
 
         static void Soma()
@@ -50,7 +51,21 @@ namespace Calculator
 
             float resultado = valor1 / valor2;
 
-            Console.WriteLine($"\nResultado da divisao: {resultado}\n");
+            Console.WriteLine($"\nResultado da divisão: {resultado}\n");
+            Console.ReadKey();
+        }
+
+        static void Multiplicacao()
+        {
+            Console.WriteLine("Primeiro valor: ");
+            float valor1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float valor2 = float.Parse(Console.ReadLine());
+
+            float resultado = valor1 * valor2;
+
+            Console.WriteLine($"\nResultado da multiplicação: {resultado}\n");
             Console.ReadKey();
         }
     }
