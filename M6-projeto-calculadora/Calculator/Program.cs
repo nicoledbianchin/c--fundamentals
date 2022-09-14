c6ba00cb781cc9b38964bec6e6e7716fe7a4da05
nicoledbianchin <nicoledbianchin@gmail.com>
2022-09-14 14:08:16 -0300

    modulo 6 - soma

--- M6-projeto-calculadora/Calculator/Program.cs
+++ M6-projeto-calculadora/Calculator/Program.cs
@@ -7,10 +7,20 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.Clear();
+
             Console.WriteLine("Primeiro valor: ");
             float valor1 = float.Parse(Console.ReadLine());
 
-            Console.WriteLine(valor1);
+            Console.WriteLine("Segundo valor: ");
+            float valor2 = float.Parse(Console.ReadLine());
+
+            float resultado = valor1 + valor2;
+
+            Console.WriteLine("");
+            Console.WriteLine("Resultado da soma: " + resultado);
+            Console.WriteLine($"Resultado da soma: {resultado}");
+            Console.WriteLine($"Resultado da soma: {valor1 + valor2}");
+            Console.WriteLine("Resultado da soma: " + (valor1 + valor2));
         }
     }
 }
