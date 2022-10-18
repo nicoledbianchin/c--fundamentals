f8b1c770e4a25695d72bf1e71f76d80f28686e1b
nicoledbianchin <nicoledbianchin@gmail.com>
2022-10-18 13:56:08 -0300

    modulo 7 - adiciona menu

--- M7-projeto-cronometro/Stopwatch/Program.cs
+++ M7-projeto-cronometro/Stopwatch/Program.cs
@@ -8,11 +8,38 @@ namespace Stopwath
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Start();
+            Menu();
         }
 
-        static void Start() {
-            int time = 10;
+        static void Menu(){
+            Console.Clear();
+            Console.WriteLine("S = segundos => 10s = 10 segundos");
+            Console.WriteLine("M = minutos => 2m = 2 minutos");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+
+            char type = char.Parse(data.Substring(data.Length - 1,1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if(type == 'm')
+            {
+                multiplier = 60;
+            } 
+
+            if(time == 0)
+            {
+               System.Environment.Exit(0);
+            }
+
+            Start(time * multiplier);
+
+            Console.WriteLine(data);
+        }
+
+        static void Start(int time) {
             int currentTime = 0;
 
             while(time != currentTime) {
@@ -21,6 +48,11 @@ namespace Stopwath
                 Console.WriteLine(currentTime);
                 Thread.Sleep(1000);
             }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado");
+            Thread.Sleep(2500);
+            Menu();
         }
     }
 }
