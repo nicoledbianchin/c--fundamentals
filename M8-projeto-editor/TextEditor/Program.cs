262e319288009c7d10ad54f7b9de47ee42134021
nicoledbianchin <nicoledbianchin@gmail.com>
2022-10-18 15:02:21 -0300

    modulo 8 - adiciona menu

--- M8-projeto-editor/TextEditor/Program.cs
+++ M8-projeto-editor/TextEditor/Program.cs
@@ -6,7 +6,43 @@ namespace TextEditor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu();
         }
+
+        static void Menu() {
+            Console.Clear();
+            Console.WriteLine("O que você deseja fazer?");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - Criar novo arquivo");
+            Console.WriteLine("0 - Sair");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    AbrirArquivo();
+                    break;
+                case 2:
+                    CriarArquivo();
+                    break;
+                case 0:
+                    Console.WriteLine("Saindo do sistema.");
+                    System.Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida");
+                    Menu();
+                    break;
+            }
+
+        }
+
+        static void AbrirArquivo(){
+
+        }
+        
+        static void CriarArquivo(){
+
+        }   
     }
 }
