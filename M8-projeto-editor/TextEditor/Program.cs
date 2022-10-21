ee68993e3061da149f09754294c30044035252fc
nicoledbianchin <nicoledbianchin@gmail.com>
2022-10-21 09:22:35 -0300

    modulo 8 abrindo arquivos

--- M8-projeto-editor/TextEditor/Program.cs
+++ M8-projeto-editor/TextEditor/Program.cs
@@ -40,7 +40,16 @@ namespace TextEditor
         }
 
         static void OpenFile(){
-
+            Console.Clear();
+            Console.WriteLine("Qual o caminho do arquivo?");
+            string path = Console.ReadLine();
+            using(var file = new StreamReader(path)){
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu();
         }
         
         static void EditFile(){
