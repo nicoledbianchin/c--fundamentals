9208cb956855cd684451060f8554fb4510cc9d73
nicoledbianchin <nicoledbianchin@gmail.com>
2022-12-05 20:05:31 -0300

    modulo 10 - finaliza menu

--- M10-projeto-editor-html/HTMLEditor/Menu.cs
+++ M10-projeto-editor-html/HTMLEditor/Menu.cs
@@ -10,6 +10,10 @@ namespace HTMLEditor
             Console.BackgroundColor = ConsoleColor.DarkCyan;
             Console.ForegroundColor = ConsoleColor.Black;
             DrawScreen(30, 10);
+            WriteOptions();
+
+            var option = short.Parse(Console.ReadLine());
+            HandleMenuOption(option);
         }
 
         private static void DrawScreen(int columns, int lines)
@@ -31,6 +35,43 @@ namespace HTMLEditor
             for(int i = 0; i <= columns; i++) Console.Write("-");
             Console.Write("+");
             Console.Write("\n");
+        }
+
+        public static void WriteOptions()
+        {
+            Console.SetCursorPosition(3,2);
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(3,3);
+            Console.WriteLine("====================");
+            Console.SetCursorPosition(3,4);
+            Console.WriteLine("Seleicone uma opção abaixo");
+            Console.SetCursorPosition(3,6);
+            Console.WriteLine("1 - Novo arquivo");
+            Console.SetCursorPosition(3,7);
+            Console.WriteLine("2 - Abrir");
+            Console.SetCursorPosition(3,9);
+            Console.WriteLine("0 - Sair");
+            Console.SetCursorPosition(3,10);
+            Console.Write("Opção: ");
+        }
+
+        public static void HandleMenuOption(short option)
+        {
+            switch(option){
+                case 1:
+                    Editor.Show();
+                    break;
+                case 2:
+                    Console.WriteLine("View");
+                    break;
+                case 0:
+                    Console.Clear();
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Show();
+                    break;
+            }
         }
     }
 }
