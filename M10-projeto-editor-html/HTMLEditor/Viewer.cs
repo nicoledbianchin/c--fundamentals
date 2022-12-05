d31d4adabc5883ce8110007a7f1c72f5e558955e
nicoledbianchin <nicoledbianchin@gmail.com>
2022-12-05 20:18:10 -0300

    modulo 10 - cria editor e viewer

---
+++ M10-projeto-editor-html/HTMLEditor/Viewer.cs
@@ -0,0 +1,29 @@
+using System.Text.RegularExpressions;
+using System;
+
+namespace HTMLEditor
+{
+    public static class Viewer
+    {
+        public static void Show(string text)
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine(" MODO DE VISUALIZAÇÃO");
+            Console.WriteLine(" ============");
+            Replace(text);
+            Console.WriteLine(" ============");
+            Console.ReadKey();
+            Menu.Show();
+        }
+
+        public static void Replace(string text)
+        {
+            var strong = new Regex(@"<\s*strong[^>]*>(.*?)<\s*/\s*strong>");
+            Console.WriteLine(strong);
+        }
+
+    }
+}
