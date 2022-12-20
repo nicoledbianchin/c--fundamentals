bc6353548a33d045aaab8f571c4af39dbdc08715
nicoledbianchin <nicoledbianchin@gmail.com>
2022-12-20 17:49:29 -0300

    modulo 12 - formatacao de moedas

--- M12-moedas/Moedas/Program.cs
+++ M12-moedas/Moedas/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Globalization;
+using System.ComponentModel.DataAnnotations;
+using System;
 
 namespace Moedas
 {
@@ -14,12 +16,64 @@ namespace Moedas
             Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.White;
             TipoParaMoeda();
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Formatando moedas");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            FormatandoMoedas();
         }
 
         static void TipoParaMoeda()
         {
             decimal valor = 10.15m; // mais comumente usado para moedas por conta da precisão
             Console.WriteLine(valor);
+        }
+
+        static void FormatandoMoedas()
+        {
+            decimal valor = 10.15m;
+
+            Console.WriteLine(valor.ToString(
+                "G",
+                CultureInfo.CreateSpecificCulture("en-US")
+            ));
+
+            Console.WriteLine(valor.ToString(
+                "G",
+                CultureInfo.CreateSpecificCulture("pt-BR")
+            ));
+
+            Console.WriteLine(valor.ToString(
+                "C",
+                CultureInfo.CreateSpecificCulture("pt-BR")
+            ));
+
+            Console.WriteLine(valor.ToString(
+                "C",
+                CultureInfo.CreateSpecificCulture("en-US")
+            ));
+
+            Console.WriteLine(valor.ToString(
+                "E04",
+                CultureInfo.CreateSpecificCulture("pt-BR")
+            ));
+
+            Console.WriteLine(valor.ToString(
+                "F",
+                CultureInfo.CreateSpecificCulture("en-US")
+            ));
+
+            Console.WriteLine(valor.ToString(
+                "N",
+                CultureInfo.CreateSpecificCulture("pt-BR")
+            ));
+
+            Console.WriteLine(valor.ToString(
+                "P",
+                CultureInfo.CreateSpecificCulture("pt-BR")
+            ));
         }
     }
 }
