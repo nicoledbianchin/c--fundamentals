c4af79fa5fdd78a535472ffac097bbc736d739e8
nicoledbianchin <nicoledbianchin@gmail.com>
2022-12-20 17:41:28 -0300

    modulo 12 - tipo para moedas

---
+++ M12-moedas/Moedas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Moedas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Tipo para moedas");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            TipoParaMoeda();
+        }
+
+        static void TipoParaMoeda()
+        {
+            decimal valor = 10.15m; // mais comumente usado para moedas por conta da precisão
+            Console.WriteLine(valor);
+        }
+    }
+}
